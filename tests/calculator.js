cc4e187388d59ff4323cb196970a07ddbc19190c
unknown <karthikkaushik91@gmail.com>
2020-04-04 14:07:14 +0530

    my first commit

---
+++ tests/calculator.js
@@ -0,0 +1,15 @@
+let homepage = require('../pages/homepage.js');
+
+describe('demo calculator tests', function(){
+
+    it ('addition test',function()
+    {
+      homepage.get('http://juliemr.github.io/protractor-demo/');
+      homepage.enterFirstNumber('4');
+      homepage.enterSecondNumber('3');
+      homepage.clickGo();
+      homepage.verifyResult('7');
+      browser.sleep(2000);
+    });
+
+});
